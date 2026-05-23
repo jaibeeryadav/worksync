033f582b41850c53b85a3f7e22c68294a0afe1f7
jaibeeryadav <jaibeeryadav8053@gmail.com>
2026-05-23 10:48:04 +0530

    basic reset password

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import URLPattern
-from.views import UserLoginView, UserLogoutView, UserRegisterView, ProfileUpdateView, ProfileDetailView
+from.views import UserLoginView, UserLogoutView, UserRegisterView, ProfileUpdateView, ProfileDetailView, DebugPasswordResetConfirmView
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('signup/', UserRegisterView.as_view(), name='signup'),
@@ -8,5 +10,12 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('profile/', ProfileDetailView.as_view(), name='profile'),
     path('profile/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
-] 
+    path("password-reset/",auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html",
+                                                                email_template_name="registration/password_reset_email.html",),name="password_reset",),
 
+    path("password-reset/done/",auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),name="password_reset_done",),
+
+    path("reset/<uidb64>/<token>/", DebugPasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"),name="password_reset_confirm",),
+
+    path("reset/done/",auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),name="password_reset_complete",),
+] 
--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,8 @@
 from .base import *
 
 DEBUG = True
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
